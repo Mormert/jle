f61d2e0f63c45f4bc7cd0d300770f472b625ad0e
Johan Lind <Github@mormert.com>
2022-11-06 12:55:25 +0100

    Emscripten build failure fix

--- engine/jle3DRenderer.cpp
+++ engine/jle3DRenderer.cpp
@@ -7,9 +7,19 @@
 #include "jleGameEngine.h"
 #include "jlePathDefines.h"
 #include "jleProfiler.h"
-#include <glad/glad.h>
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
+
+#ifdef __EMSCRIPTEN__
+#include <GLES3/gl3.h>
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+
+#include <glad/glad.h>
+
+#endif
 
 #include <random>
 
