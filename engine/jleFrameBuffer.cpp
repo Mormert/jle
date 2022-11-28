2f2e37d22945d05dcd3fdfdf4a6120bab1ac7543
Johan Lind <Github@mormert.com>
2022-11-28 11:04:04 +0100

    WASM Compile Error fix continue

--- engine/jleFrameBuffer.cpp
+++ engine/jleFrameBuffer.cpp
@@ -3,8 +3,9 @@
 #include "jleFrameBuffer.h"
 
 #ifdef __EMSCRIPTEN__
-#include <GLES2/gl2ext.h>
 #include <GLES3/gl3.h>
+
+#include <GLES2/gl2ext.h>
 
 #include <emscripten.h>
 #define GL_GLEXT_PROTOTYPES
