3d045f91b80135782c86422a9235b78c5a3a574c
Johan Lind <Github@mormert.com>
2023-07-14 11:09:41 +0200

    Error notification duration

--- engine/editor/jleEditorNotifications.cpp
+++ engine/editor/jleEditorNotifications.cpp
@@ -3,27 +3,24 @@
 #include "jleEditorNotifications.h"
 #include "jleDynamicLogAppender.h"
 
-jleEditorNotifications::jleEditorNotifications(const std::string& windowName)
-    : iEditorImGuiWindow(windowName)
+jleEditorNotifications::jleEditorNotifications(const std::string &windowName) : iEditorImGuiWindow(windowName)
 {
     dynamicAppender().addAppender(this);
 
     _errorImage = jleResourceRef<jleTexture>(jlePath{"ED:icons/error.png"});
 }
 
-jleEditorNotifications::~jleEditorNotifications() {
-    dynamicAppender().removeAppender(this);
-}
+jleEditorNotifications::~jleEditorNotifications() { dynamicAppender().removeAppender(this); }
 
 void
 jleEditorNotifications::update(jleGameEngine &ge)
 {
     using namespace std::chrono;
-    long long now = system_clock::now().time_since_epoch().count();
+    long long now = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 
-    for(int i = _notifications.size() - 1; i >= 0; i--){
-        auto&& notification = _notifications[i];
-        if(notification.activeUntil < now){
+    for (int i = _notifications.size() - 1; i >= 0; i--) {
+        auto &&notification = _notifications[i];
+        if (notification.activeUntil < now) {
             _notifications.erase(std::begin(_notifications) + i);
         }
     }
@@ -37,21 +34,19 @@ jleEditorNotifications::update(jleGameEngine &ge)
     auto x = ImGui::GetWindowViewport()->Pos.x + 45;
     auto y = ImGui::GetWindowViewport()->Pos.y + 45;
 
-    for(const auto& notification : _notifications){
+    for (const auto &notification : _notifications) {
 
         std::wstring first64 = notification.message.substr(0, 64);
-        if(first64.size() == 64)
-        {
+        if (first64.size() == 64) {
             first64 += L"...";
         }
 
         ImGui::SetNextWindowSize(ImVec2{400 * globalImguiScale, 26 * globalImguiScale});
         ImGui::SetNextWindowPos(ImVec2{x, y + 32.f * globalImguiScale * i});
-        ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove |
-                                 ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse |
-                                 ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoMouseInputs | ImGuiWindowFlags_NoDocking;
+        ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize |
+                                 ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoFocusOnAppearing |
+                                 ImGuiWindowFlags_NoMouseInputs | ImGuiWindowFlags_NoDocking;
         ImGui::Begin(std::to_string(i).c_str(), &t, flags);
-
 
         ImGui::Image((void *)(intptr_t)_errorImage->id(),
                      ImVec2(16 * globalImguiScale, 16 * globalImguiScale),
@@ -70,26 +65,28 @@ jleEditorNotifications::update(jleGameEngine &ge)
     ImGui::PopStyleColor();
 }
 
-void jleEditorNotifications::addNotificationError(const std::wstring& message){
+void
+jleEditorNotifications::addNotificationError(const std::wstring &message)
+{
     using namespace std::chrono;
-    long long now = system_clock::now().time_since_epoch().count();
 
-    _notifications.push_back({message, now + 30000000});
+    long long now = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+
+    _notifications.push_back({message, now + 8000});
 }
 
 void
 jleEditorNotifications::write(const plog::Record &record)
 {
-    if(record.getSeverity() == plog::Severity::error){
+    if (record.getSeverity() == plog::Severity::error) {
         std::wstring m = record.getMessage();
         addNotificationError(m);
         return;
     }
 
-    if(record.getSeverity() == plog::Severity::fatal){
+    if (record.getSeverity() == plog::Severity::fatal) {
         std::wstring m = record.getMessage();
         addNotificationError(m);
         return;
     }
-
 }
