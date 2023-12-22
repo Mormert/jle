b719cfe598e4db77de70ba05b6877488d1f2a593
Johan Lind <Github@mormert.com>
2023-12-22 23:52:19 +0100

    Shorter network event name using string hashing at compile time

--- engine/jleNetworkEvent.cpp
+++ engine/jleNetworkEvent.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2023. Johan Lind
 
 #include "jleNetworkEvent.h"
+#include "jleSceneServer.h"
 
 template <class Archive>
 void
@@ -9,3 +10,9 @@ jleNetworkEvent::serialize(Archive &archive)
 }
 
 JLE_EXTERN_TEMPLATE_CEREAL_CPP(jleNetworkEvent)
+
+jleSceneServer &
+jleNetworkEvent::getSceneServer()
+{
+    return *_serverScene;
+}
