e7220e2eb2d02efbaecfa88dd8e76635a0b63b1b
Johan Lind <Github@mormert.com>
2023-12-27 23:00:38 +0100

    event name hashing bug fixes

--- engine/jleNetworkEventRegistrator.h
+++ engine/jleNetworkEventRegistrator.h
@@ -8,7 +8,10 @@
 #include <string_view>
 
 #ifndef NDEBUG
-#define JLE_REGISTER_NET_EVENT_HASH_COLLIDE_CHECK(T) static inline jleNetNameTransform::HashCollideCheck T##_checker(#T);
+#include <chrono>
+#include <thread>
+#define JLE_REGISTER_NET_EVENT_HASH_COLLIDE_CHECK(T)                                                                   \
+    static inline jleNetNameTransform::HashCollideCheck T##_checker(#T);
 #else
 #define JLE_REGISTER_NET_EVENT_HASH_COLLIDE_CHECK(T)
 #endif
@@ -62,12 +65,12 @@
 namespace jleNetNameTransform
 {
 
-using HashType = std::array<char, 4>;
+using HashType = std::array<char, 5>;
 
 constexpr uint32_t
 djb2(const char *str)
 {
-    uint32_t hash = 5381;
+    uint64_t hash = 5381;
     while (*str) {
         hash = (hash << 5) + *str++;
     }
@@ -83,19 +86,16 @@ fnv1a_hash(const char *str, std::size_t hash = 14695981039346656037ull) noexcept
 constexpr HashType
 createHash(std::string_view str)
 {
-    // A function that returns 4 bytes of a hash from an input string, where the last byte is a null-terminator
+    // A function that returns X bytes of a hash from an input string, where the last byte is a null-terminator
 
-    auto hash = djb2(str.data());
+    auto hash = fnv1a_hash(str.data());
     HashType ret{};
-    for (int i = 0; i < sizeof(HashType); ++i) {
+    for (int i = 0; i < 4; ++i) {
         ret[i] = static_cast<char>((hash >> (i * 8)) & 0xFF);
     }
 
-    // Drop the last byte and set null terminator, and make the other bytes depend on last byte to make it significant
-    ret[0] *= ret[3];
-    ret[1] += ret[3];
-    ret[2] -= ret[3];
-    ret[3] = '\0';
+    // Set null terminator, and make the other bytes depend on last byte to make it significant
+    ret[4] = '\0';
 
     return ret;
 }
@@ -103,7 +103,7 @@ createHash(std::string_view str)
 template <typename T>
 struct HashedEventName {
     // This is used to shorten the event name from the original class name "SomeLargeNameForAnEvent"
-    // to a short 4 byte, 3-character long version, like "+£?" with a null-terminator byte at the end.
+    // to a short X byte, X-1 character long version, like "+£?" with a null-terminator byte at the end.
     // This is done at compile time.
     static constexpr HashType hash = createHash(T::name);
 };
@@ -131,7 +131,14 @@ struct HashCollideCheck {
 
         for (auto &collideCheck : hashCollideChecker) {
             if (collideCheck.hashResult == h) {
-                std::cerr << "Found two identical hashes for event with name: " << str;
+#ifdef WIN32
+                __debugbreak();
+#endif
+                int i = *reinterpret_cast<int *>(&h[0]);
+                std::cerr << "Found two identical hashes (" << i << ") for event with name: " << str << ", "
+                          << collideCheck.hashName << std::endl;
+                using namespace std::chrono_literals;
+                std::this_thread::sleep_for(100ms);
                 std::exit(1);
             }
         }
