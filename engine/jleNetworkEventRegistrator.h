319a161994cbcb9601bd0090d7a18b700aa657af
Johan Lind <Github@mormert.com>
2023-12-27 17:46:49 +0100

    bug fixes to net event name hashing

--- engine/jleNetworkEventRegistrator.h
+++ engine/jleNetworkEventRegistrator.h
@@ -8,7 +8,8 @@
 #include <string_view>
 
 // Use this macro to register networked events, and shorten the serialized name for the event using hashing.
-// This works for registering both client to server events, and server to client events as well as any other networked event.
+// This works for registering both client to server events, and server to client events as well as any other networked
+// event.
 #define JLE_REGISTER_NET_EVENT(T)                                                                                      \
     namespace cereal                                                                                                   \
     {                                                                                                                  \
@@ -22,6 +23,7 @@
         CEREAL_STATIC_CONSTEXPR char const *                                                                           \
         name()                                                                                                         \
         {                                                                                                              \
+                                                                                                                       \
             constexpr const char *hashedName = jleNetNameTransform::HashedEventName<HashName>::hash.data();            \
             return hashedName;                                                                                         \
         }                                                                                                              \
@@ -43,38 +45,52 @@
             } else if constexpr (std::is_base_of<jleServerToClientEvent, T>::value) {                                  \
                 RegisterPolymorphicCaster<jleServerToClientEvent, T>::bind();                                          \
             } else {                                                                                                   \
-                RegisterPolymorphicCaster<jleNetworkEvent, T>::bind();                                               \
+                RegisterPolymorphicCaster<jleNetworkEvent, T>::bind();                                                 \
             }                                                                                                          \
         }                                                                                                              \
     };                                                                                                                 \
     }                                                                                                                  \
-    }
+    }                                                                                                                  \
+    static inline jleNetNameTransform::HashCollideCheck T##_checker(#T);
 
 namespace jleNetNameTransform
 {
 
-constexpr size_t
+using HashType = std::array<char, 4>;
+
+constexpr uint32_t
 djb2(const char *str)
 {
-    size_t hash = 5381;
+    uint32_t hash = 5381;
     while (*str) {
         hash = (hash << 5) + *str++;
     }
     return hash;
 }
 
-constexpr std::array<char, 4>
+constexpr std::size_t
+fnv1a_hash(const char *str, std::size_t hash = 14695981039346656037ull) noexcept
+{
+    return (str[0] == '\0') ? hash : fnv1a_hash(&str[1], (hash ^ static_cast<std::size_t>(str[0])) * 1099511628211ull);
+}
+
+constexpr HashType
 createHash(std::string_view str)
 {
     // A function that returns 4 bytes of a hash from an input string, where the last byte is a null-terminator
 
-    size_t l = djb2(str.data());
-    std::array<char, 4> ret{};
-    for (int i = 0; i < 3; i++) {
-        ret[i] = static_cast<uint8_t>(l & 0xFF);
-        l >>= 8;
+    auto hash = djb2(str.data());
+    HashType ret{};
+    for (int i = 0; i < sizeof(HashType); ++i) {
+        ret[i] = static_cast<char>((hash >> (i * 8)) & 0xFF);
     }
+
+    // Drop the last byte and set null terminator, and make the other bytes depend on last byte to make it significant
+    ret[0] *= ret[3];
+    ret[1] += ret[3];
+    ret[2] -= ret[3];
     ret[3] = '\0';
+
     return ret;
 }
 
@@ -83,7 +99,41 @@ struct HashedEventName {
     // This is used to shorten the event name from the original class name "SomeLargeNameForAnEvent"
     // to a short 4 byte, 3-character long version, like "+£?" with a null-terminator byte at the end.
     // This is done at compile time.
-    static constexpr std::array<char, 4> hash = createHash(T::name);
+    static constexpr HashType hash = createHash(T::name);
 };
+
+#ifndef NDEBUG
+
+struct HashCollideCheckData {
+    std::string hashName;
+    HashType hashResult;
+};
+
+static inline std::vector<HashCollideCheckData> hashCollideChecker;
+
+struct HashCollideCheck {
+    explicit HashCollideCheck(const char *str)
+    {
+        for (auto &collideCheck : hashCollideChecker) {
+            if (collideCheck.hashName == str) {
+                // This name is already registered, skipping...
+                return;
+            }
+        }
+
+        auto h = createHash(str);
+
+        for (auto &collideCheck : hashCollideChecker) {
+            if (collideCheck.hashResult == h) {
+                std::cerr << "Found two identical hashes for event with name: " << str;
+                std::exit(1);
+            }
+        }
+
+        hashCollideChecker.push_back({std::string{str}, h});
+    }
+};
+
+#endif
 
 } // namespace jleNetNameTransform
