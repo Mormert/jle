82ceba6c128956e6a8553aef0d7078a18ffaeadf
Johan Lind <Github@mormert.com>
2024-03-31 00:13:40 +0100

    Lua improvements, definition files for cpp components, improve add and get lua bindings

---
+++ engine/EngineResources/scripts/definitions/components/cLightDirectional.lua
@@ -0,0 +1,10 @@
+---@meta
+
+---@class cLightDirectional : cLight
+cLightDirectional = {}
+
+---@return cLightDirectional
+function jleObject:addComponent_cLightDirectional() end
+
+---@return cLightDirectional
+function jleObject:getComponent_cLightDirectional() end
